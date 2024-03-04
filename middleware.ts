e9913aac2f134f331443e2033661cad41619592d
김주현 <prokion33@gmail.com>
2024-03-04 14:40:29 +0900

    미들웨어  설정

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,5 @@
 export { default } from "next-auth/middleware";
 
 export const config = {
-  matcher: ["/stores/new", "/stores/:id/edit", "/users/likes"],
+  matcher: ["/profile", "/stores/:id/edit", "/users/likes"],
 };
--- pages/profile.tsx
+++ pages/profile.tsx
@@ -2,25 +2,21 @@ import UserProfile from "../components/profile/UserProfile";
 import { getSession } from "next-auth/react";
 import { GetServerSidePropsContext } from "next";
 
-function ProfilePage(props) {
-console.log(props.session)
-  return (<UserProfile/>)
-  
- 
+function ProfilePage() {
+  return <UserProfile />;
 }
-export async function getServerSideProps(context:GetServerSidePropsContext) {
-  const session = await getSession({ req: context.req });
-  if (!session) {
-    return {
-      
-      redirect:{
-        destination:'/auth',
-        permanent:false
-      }
-    };
-  }
-  return {
-    props:{session}
-  }
-}
+// export async function getServerSideProps(context: GetServerSidePropsContext) {
+//   const session = await getSession({ req: context.req });
+//   if (!session) {
+//     return {
+//       redirect: {
+//         destination: "/auth",
+//         permanent: false,
+//       },
+//     };
+//   }
+//   return {
+//     props: { session },
+//   };
+// }
 export default ProfilePage;
