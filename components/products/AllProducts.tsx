6497f095d550a3158e66993454f3f6ac7de93b36
김주현 <prokion33@gmail.com>
2024-08-22 15:01:25 +0900

    타입수정

--- components/products/AllProducts.tsx
+++ components/products/AllProducts.tsx
@@ -10,12 +10,12 @@ interface AllProductsProps{
 function AllProducts(props:AllProductsProps) {
   const { products } = props;
   
-  const [lists, setLists] = useState([]); // 백엔드와 통신하여 모든 데이터를 setLists 에 저장해서 사용
+  const [lists , setLists] = useState<IProduct[]>([]); // 백엔드와 통신하여 모든 데이터를 setLists 에 저장해서 사용
   const [limit, setLimit] = useState(5); // 한 페이지에 보여줄 데이터의 개수
   const [page, setPage] = useState(1); // 페이지 초기 값은 1페이지
   const [counts, setCounts] = useState(1); // 데이터의 총 개수를 setCounts 에 저장해서 사용
   const [blockNum, setBlockNum] = useState(0); // 한 페이지에 보여 줄 페이지네이션의 개수를 block으로 지정하는 state. 초기 값은 0
-  const [visibleLists, setVisibleLists] = useState([]);
+  const [visibleLists, setVisibleLists] = useState<IProduct[]>([]);
   useEffect(()=>{setLists(products);setCounts(products.length)},[products])
   
   return (
